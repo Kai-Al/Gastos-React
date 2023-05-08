4cc349b40894f81188283b3d57369fd8f7533ef6
Kai-Al <Kai.albornoz@udea.edu.co>
2023-05-08 17:21:12 -0500

    Feat: calculate Balance

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -2,10 +2,15 @@ import { useGlobalState } from "../context/GlobalState"
 
 function Balance() {
 
-    const  transactions  = useGlobalState();
+    const  {transactions}  = useGlobalState();
+
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    
     return (
         <div>
             <h1>Balance</h1>
+            <h2>${total}</h2>
         </div>
     )
 }
