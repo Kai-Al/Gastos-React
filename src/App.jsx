c748918c48a49e9b70822444e8d307275ddefdf7
Kai-Al <Kai.albornoz@udea.edu.co>
2023-05-08 17:27:32 -0500

    Feat: Income and Expenses

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import Header from "./components/Header";
 import Balance from "./components/Balance";
 import TransactionForm from "./components/Transactions/TransactionForm";
 import TransactionList from "./components/Transactions/TransactionList";
+import IncomeExpenses from "./components/IncomeExpenses";
 
 function App() {
   return (
     <GlobalProvider>
       <div className="App">
         <Header />
+        <IncomeExpenses />
         <Balance />
         <TransactionForm />
         <h1>Expense Tracker</h1>
         <TransactionList />
-        <h1>React Context API</h1>
       </div>
     </GlobalProvider>
   );
